91bef48697a111d3d2f5b467df6c61b46b8f7fb3
少爷宠爱一生 <1428319670@qq.com>
2023-09-08 16:01:25 +0800

    新增音乐播放器

---
+++ audio/mediaList.js
@@ -0,0 +1,58 @@
+export const mediaList = [
+  {
+    value: 0,
+    label: 'See Tinh - hoang Thuy Linh'
+  },
+  {
+    value: 1,
+    label: '如果当时 - 许嵩'
+  },
+  {
+    value: 2,
+    label: '这世界那么多人 - 莫文蔚'
+  },
+  {
+    value: 3,
+    label: '哪里都是你 - 队长'
+  },
+  {
+    value: 4,
+    label: '圈住你 - 一口甜'
+  },
+  {
+    value: 5,
+    label: '归期 - 钱润玉'
+  },
+  {
+    value: 6,
+    label: '就现在 - 吴莫愁'
+  },
+  {
+    value: 7,
+    label: '乌梅子酱 - 李荣浩'
+  },
+  {
+    value: 8,
+    label: '大城小爱 - 王力宏'
+  },
+  {
+    value: 9,
+    label: '春娇与志明 - 街道办GDC,欧阳耀莹'
+  },
+  {
+    value: 10,
+    label: '醉清风 - 弦子'
+  },
+  {
+    value: 11,
+    label: '阿拉斯加海湾 - 蓝心羽'
+  },
+  {
+    value: 12,
+    label: '情话 - 徐良'
+  },
+  {
+    value: 13,
+    label: 'cupid - Lil C.C.'
+  }
+]
